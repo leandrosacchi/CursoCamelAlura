88cc6c43fcd19291b802f0592dc80ba713ea5459
leandro-gft <lpsacchi@gmail.com>
2020-05-15 13:43:19 -0300

    Conectando endpoints HTTP

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,9 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -15,16 +17,22 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true"). //pasta de origem, o noop é utilizado para que os arquivos não sejam apagados da pasta de origem
+				setProperty("pedidoId", xpath("/pedido/id/text()")).
+				setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
 				split().			//separa a mensagem por item
-					xpath("/pedido/itens/item").
+					xpath("/pedido/itens/item").	//navega por entre as tags no formato xml
 				filter().			//filtra apenas os EBOOKS
 					xpath("/item/formato[text()='EBOOK']").
-				log("${id}").	//imprime os ids no console
+				setProperty("ebookId", xpath("/item/livro/codigo/text()")).
 				marshal().xmljson().//transforma arquivo xml em json
-				log("${body}").	//imprime o corpo das mensagens no console
-				setHeader("CamelFileName", simple("${file:name.noext}.json")). //renomeia os arquivos tirando a extensão original e substituindo por .json
-				to("file:saida");	//pasta de destino
+				log("${id} - ${body}").	//imprime o id e corpo das mensagens no console
+				setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+				setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
 				
+			//	setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")). //renomeia os arquivos tirando a extensão original e substituindo por .json
+				// o ${header.CamelSplitIndex} foi utilizado para separar cada EBOOK em um json diferente.
+				to("http4://localhost:8080/webservices/ebook/item");	//pasta de destino
+			
 			}
 		});
 		
